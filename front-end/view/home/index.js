56c1bfd3da0c1e76932b3bd1ce625a70375a928f
Ambre GONDRY <gondry.ambre@gmail.com>
2021-05-24 21:47:33 +0200

    correction index/page produit/ création formulaire page panier

--- front-end/view/home/index.js
+++ front-end/view/home/index.js
@@ -1,35 +1,36 @@
+//Url de l'API des produit selectionnés(ici les appareils photos)
+const url = 'http://localhost:3000/api/cameras';
 
 //Affichage des produits dans l'HTML
-const displayItem = item => {
-    let main = document.getElementById('main');
-    main.innerHTML += `<aside id=${item._id} class="item product-cards">
-                            <figure class="product-cards__prev">
-                                <img src="${item.imageUrl}" alt="${item.name}">
+const displayAllCameras = camera => {
+        document.getElementById('main')
+                .innerHTML += `<aside id=${camera._id} class="item product-cards">
+                                <figure class="product-cards__prev">
+                                    <img src="${camera.imageUrl}" alt="${camera.name}">
                                 <figcaption class="product-cards__details">
-                                    <h2>  ${item.name} </h2>
-                                    <p class="price">Prix : <b>${item.price/100} €</b></p>
+                                    <h2>  ${camera.name} </h2>
+                                    <p class="price">Prix : <b>${camera.price/100} €</b></p>
                                     <button>
-                                        <a class="details" href="../product/product.html?id=${item._id}">
+                                        <a class="details" href="../product/product.html?id=${camera._id}">
                                             En savoir plus sur cet appareil
                                         </a>
                                     </button>
                                 </figcaption>
-                            </figure>
-                        </aside>`;
+                                </figure>
+                              </aside>`;
+                        
 
-    console.log(item);
-    console.log(`Id : ${item._id} - Nom : ${item.name}`);
+    console.log(camera);
+    console.log(`Id : ${camera._id} - Nom : ${camera.name}`);
 }
 
 //Recuperer les données de l'API
-const getInfo = async() =>{
-    let response = await fetch('http://localhost:3000/api/cameras');
-    let data = await response.json();
-    data.map(item => displayItem(item))
+const getCameraInfo = async() =>{
+    let res = await fetch(url);
+    let data = await res.json();
+    data.map(camera => displayAllCameras(camera))
 }
 
 //Appel des données
-getInfo();
-
-
+getCameraInfo();
 
--- front-end/view/product/product.js
+++ front-end/view/product/product.js
@@ -5,15 +5,14 @@ console.log(cameraId)
 
 
 //affichage de l'appareil selectionné
-const displayCamera = camera => {
-    let id = cameraId ;
-     fetch(`http://localhost:3000/api/cameras/${id}`)
-        .then(function(res) {
+const displayOneCamera = () => {
+     fetch(`http://localhost:3000/api/cameras/${cameraId}`)//on appelle les données correspondant à l'appareil selectionné
+        .then(function(res) {//si la requête a fonctionné elle nous retourne le résultat au format JSON
             if (res.ok) {
             return res.json();
             }
         })
-        .then(function(value) {
+        .then(function(value) {//nous recupérons la valeur de la requête précedente et l'affichons dans le container "product"
             console.log(value);
             document
                 .getElementById('product')
@@ -24,13 +23,11 @@ const displayCamera = camera => {
                                             <h1>  ${value.name} </h1>
                                             <p class="description"> ${value.description} </p>
                                             <select class="lenses"> 
-                                                    <option> ${value.lenses[0]} </option>
-                                                    <option> ${value.lenses[1]} </option>
-                                                    <option> ${value.lenses[2]} </option>
+                                                    <option>Choisissez votre Lentille.. ${value.lenses.map((lenses) => `<option>${lenses}</option>`)} </option>
                                             </select>
                                             <p class="price">Prix unitaire :<b> ${value.price/100} €</b></p>
                                             <button>
-                                                <a id="addToCart">
+                                                <a id="addToCart" href ="../cart/cart.html">
                                                     Ajouter au Panier
                                                 </a>
                                             </button>
@@ -39,7 +36,26 @@ const displayCamera = camera => {
                                 </aside>`;
         })
         .catch(function(err) {
-            alert("une erreur s'est produite")
+            document
+                .getElementById('product')
+                .innerHTML += `<aside class="error">
+                                    <h2>
+                                        Une erreur s'est produite, Veuillez nous en excuser et recharger la page ultérieurement
+                                    </h2>
+                                </aside>`;
+        
+        
+        const addToCart = document.getElementById("addToCart");
+
+                addToCart.addEventListener("click", () => {
+                const cartContent = JSON.parse(localStorage.getItem("cartContent")) || []; 
+                    
+                    cartContent.push(cameraId);
+                    localStorage.setItem("cartContent", JSON.stringify(cartContent)); 
+  
   });
+})
+
 }
-displayCamera()
+
+displayOneCamera()
